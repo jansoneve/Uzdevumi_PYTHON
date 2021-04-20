65a299e577de030a2752f1f53462f06d8049e8ab
jansoneve <jansoneve@gmail.com>
2021-04-20 10:36:55 +0300

    2.uzd

--- u012_input_laukums.py
+++ u012_input_laukums.py
@@ -4,3 +4,11 @@ trijstūra augstumu un pamatu
 un izvada uz ekrāna (print) trijstūra laukumu.
 Pārbaudiet programmas darbību ar dažādiem ievaddatiem.
 """
+
+augstums=float(input("Ievadi trijstūra augstumu:"))
+pamats=float(input("Ievadi trijstūra pamatu:"))
+
+sk3=augstums*pamats
+laukums=sk3/2
+
+print("Trijstūra laukums ir ",laukums)
