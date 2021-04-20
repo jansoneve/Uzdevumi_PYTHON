d3d9b0283024a6f9591e79b1132a0781c889bb18
jansoneve <jansoneve@gmail.com>
2021-04-20 11:22:44 +0300

    5.uzd

--- u023_hipotenuza.py
+++ u023_hipotenuza.py
@@ -1,7 +1,14 @@
 import math
 
 def hipotenuza(a, b):
-    """
+    hipotenuza=math.sqrt(pow(a,2)+pow(b,2))
+    return hipotenuza
+if (b<=0) or (a<=0):
+    print(0)
+    break
+
+print(hipotenuza(3, 4))
+"""
     Funkcija akceptē divus argumentus - katešu garumus un atgriež 
     hipotenūzas garumu, vai 0, ja kaut vienas katetes garums ir <= 0
     Pārbaudiet funkcijas darbību ar dažādiem argumentiem.
